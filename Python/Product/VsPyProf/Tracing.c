c28390b6d0ef3d8e85905c543aa96899eaa13c3b
Trent Nelson <trent@trent.me>
2015-12-08 20:21:56 -0500

    Checkpoint commit of tracing work.  Work in progress.

---
+++ Python/Product/VsPyProf/Tracing.c
@@ -0,0 +1,351 @@
+// Python Tools for Visual Studio
+// Copyright(c) Microsoft Corporation
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the License); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+// IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+//
+// See the Apache Version 2.0 License for specific language governing
+// permissions and limitations under the License.
+
+#include <Windows.h>
+#include <Strsafe.h>
+#include "Tracing.h"
+
+INIT_ONCE InitOnceFindLongestTraceStoreFileName = INIT_ONCE_STATIC_INIT;
+
+BOOL
+CALLBACK
+FindLongestTraceStoreFileNameCallback(
+    PINIT_ONCE InitOnce,
+    PVOID Parameter,
+    PVOID *lpContext
+)
+{
+    DWORD Index;
+    LARGE_INTEGER Longest = { 0 };
+    LARGE_INTEGER Length = { 0 };
+
+    for (Index = 0; Index < NumberOfTraceStores; Index++) {
+        LPCWSTR FileName = TraceStoreFileNames[Index];
+        if (FAILED(StringCchLengthW(FileName, MAX_PATH-5, &Length.QuadPart))) {
+            return FALSE;
+        }
+        if (Length.QuadPart > Longest.QuadPart) {
+            Longest.QuadPart = Length.QuadPart;
+        }
+    }
+
+    *((PDWORD64)lpContext) = Longest.QuadPart;
+
+    return TRUE;
+}
+
+DWORD
+GetLongestTraceStoreFileName()
+{
+    BOOL  Status;
+    LARGE_INTEGER Longest;
+
+    Status = InitOnceExecuteOnce(
+        &InitOnceFindLongestTraceStoreFileName,
+        FindLongestTraceStoreFileNameCallback,
+        NULL,
+        (LPVOID *)&Longest.QuadPart
+    );
+
+    if (!Status || Longest.HighPart != 0) {
+        return 0;
+    } else {
+        return Longest.LowPart;
+    }
+}
+
+BOOL
+RefreshTraceStoreFileInfo(PTRACE_STORE TraceStore)
+{
+    if (!TraceStore) {
+        return FALSE;
+    }
+
+    return GetFileInformationByHandleEx(
+        TraceStore->FileHandle,
+        (FILE_INFO_BY_HANDLE_CLASS)FileStandardInfo,
+        &TraceStore->FileInfo,
+        sizeof(TraceStore->FileInfo)
+    );
+}
+
+BOOL
+InitializeTraceStore(
+    LPCWSTR Path,
+    PTRACE_STORE TraceStore,
+    DWORD InitialSize
+)
+{
+    BOOL Success;
+    TraceStore->FileHandle = CreateFileW(
+        Path,
+        GENERIC_READ | GENERIC_WRITE,
+        FILE_SHARE_READ,
+        NULL,
+        CREATE_ALWAYS,
+        FILE_FLAG_OVERLAPPED,
+        NULL
+    );
+
+    if (TraceStore->FileHandle == INVALID_HANDLE_VALUE) {
+        goto error;
+    }
+
+    if (!RefreshTraceStoreFileInfo(TraceStore)) {
+        goto error;
+    }
+
+    TraceStore->MappingSize.HighPart = 0;
+    TraceStore->MappingSize.LowPart = InitialSize;
+
+    // If the allocated size of the underlying file is less than our desired
+    // mapping size (which is primed by the InitialSize parameter), extend the
+    // file to that length (via SetFilePointerEx(), then SetEndOfFile()).
+    if (TraceStore->FileInfo.AllocationSize.QuadPart < TraceStore->MappingSize.QuadPart) {
+        LARGE_INTEGER StartOfFile = { 0 };
+        Success = SetFilePointerEx(
+            TraceStore->FileHandle,
+            TraceStore->MappingSize,
+            NULL,
+            FILE_BEGIN
+        );
+        if (!Success) {
+            goto error;
+        }
+        // Extend the file.
+        if (!SetEndOfFile(TraceStore->FileHandle)) {
+            goto error;
+        }
+        // Reset the file pointer back to the start.
+        Success = SetFilePointerEx(
+            TraceStore->FileHandle,
+            StartOfFile,
+            NULL,
+            FILE_BEGIN
+        );
+        if (!Success) {
+            goto error;
+        }
+    }
+
+    TraceStore->MappingHandle = CreateFileMapping(
+        TraceStore->FileHandle,
+        NULL,
+        PAGE_READWRITE,
+        0,
+        TraceStore->MappingSize.LowPart,
+        NULL
+    );
+
+    if (TraceStore->MappingHandle == INVALID_HANDLE_VALUE) {
+        goto error;
+    }
+
+    TraceStore->BaseAddress = MapViewOfFile(
+        TraceStore->MappingHandle,
+        FILE_MAP_READ | FILE_MAP_WRITE,
+        0,
+        0,
+        TraceStore->MappingSize.LowPart
+    );
+
+    if (!TraceStore->BaseAddress) {
+        goto error;
+    }
+
+    TraceStore->NextAddress = TraceStore->BaseAddress;
+
+    return TRUE;
+error:
+    CloseTraceStore(TraceStore);
+    return FALSE;
+}
+
+BOOL
+InitializeTraceStores(
+    LPWSTR BaseDirectory,
+    PTRACE_STORES TraceStores,
+    LPDWORD InitialFileSizes
+)
+{
+    BOOL Success;
+    HRESULT Result;
+    DWORD Index;
+    DWORD LastError;
+    WCHAR Path[MAX_PATH];
+    LPWSTR FileNameDest;
+    DWORD LongestFilename = GetLongestTraceStoreFileName();
+    DWORD LongestPossibleDirectoryLength = (
+        MAX_PATH -
+        1        - // '\'
+        1        - // final NUL
+        LongestFilename
+    );
+    LARGE_INTEGER DirectoryLength;
+    LARGE_INTEGER RemainingChars;
+    LPDWORD Sizes = InitialFileSizes;
+
+    if (!BaseDirectory || !TraceStores) {
+        SetLastError(ERROR_INVALID_PARAMETER);
+        return FALSE;
+    }
+
+    if (!Sizes) {
+        Sizes = (LPDWORD)&InitialTraceStoreFileSizes[0];
+    }
+
+    Result = StringCchLengthW(
+        BaseDirectory,
+        LongestPossibleDirectoryLength,
+        &DirectoryLength.QuadPart
+    );
+
+    if (FAILED(Result)) {
+        SetLastError(ERROR_INVALID_PARAMETER);
+        return FALSE;
+    }
+
+    if (DirectoryLength.HighPart != 0) {
+        SetLastError(ERROR_INVALID_PARAMETER);
+        return FALSE;
+    }
+
+    SecureZeroMemory(&Path, sizeof(Path));
+
+    Result = StringCchCopyW(
+        &Path[0],
+        LongestPossibleDirectoryLength,
+        BaseDirectory
+    );
+    if (FAILED(Result)) {
+        return ERROR_INVALID_PARAMETER;
+    }
+
+    Path[DirectoryLength.LowPart] = L'\\';
+    FileNameDest = &Path[DirectoryLength.LowPart+1];
+    RemainingChars.QuadPart = (
+        MAX_PATH -
+        DirectoryLength.LowPart -
+        2
+    );
+
+    SecureZeroMemory(TraceStores, sizeof(TraceStores));
+
+    Success = CreateDirectory(BaseDirectory, NULL);
+    if (!Success) {
+        LastError = GetLastError();
+        if (LastError != ERROR_ALREADY_EXISTS) {
+            SetLastError(LastError);
+            return FALSE;
+        }
+    }
+
+    for (Index = 0; Index < NumberOfTraceStores; Index++) {
+        PTRACE_STORE TraceStore = &TraceStores->Stores[Index];
+        LPCWSTR FileName = TraceStoreFileNames[Index];
+        DWORD InitialSize = Sizes[Index];
+        Result = StringCchCopyW(FileNameDest, RemainingChars.QuadPart, FileName);
+        if (FAILED(Result)) {
+            SetLastError(ERROR_INSUFFICIENT_BUFFER);
+            return FALSE;
+        }
+        Result = InitializeTraceStore(Path, TraceStore, InitialSize);
+        if (FAILED(Result)) {
+            return FALSE;
+        }
+    }
+
+    return TRUE;
+}
+
+void
+CloseTraceStore(PTRACE_STORE TraceStore)
+{
+    if (!TraceStore) {
+        return;
+    }
+
+    if (TraceStore->BaseAddress) {
+        UnmapViewOfFile(TraceStore->BaseAddress);
+        TraceStore->BaseAddress = NULL;
+    }
+
+    if (TraceStore->MappingHandle) {
+        CloseHandle(TraceStore->MappingHandle);
+        TraceStore->MappingHandle = NULL;
+    }
+
+    if (TraceStore->FileHandle) {
+        CloseHandle(TraceStore->FileHandle);
+        TraceStore->FileHandle = NULL;
+    }
+}
+
+void
+CloseTraceStores(PTRACE_STORES TraceStores)
+{
+    DWORD Index;
+
+    if (!TraceStores) {
+        return;
+    }
+
+    for (Index = 0; Index < NumberOfTraceStores; Index++) {
+        CloseTraceStore(&TraceStores->Stores[Index]);
+    }
+}
+
+BOOL
+GetTraceStoreBytesWritten(
+    PTRACE_STORE TraceStore,
+    PLARGE_INTEGER BytesWritten
+)
+{
+    if (!TraceStore || !TraceStore->BaseAddress || !BytesWritten) {
+        return FALSE;
+    }
+
+    BytesWritten->QuadPart = (
+        (DWORD_PTR)TraceStore->NextAddress -
+        (DWORD_PTR)TraceStore->BaseAddress
+    );
+
+    return TRUE;
+}
+
+BOOL
+GetTraceStoreRecordCount(
+    PTRACE_STORE TraceStore,
+    PLARGE_INTEGER RecordCount
+)
+{
+    if (!TraceStore) {
+        return FALSE;
+    }
+
+    RecordCount->QuadPart = TraceStore->RecordCount.QuadPart;
+    return TRUE;
+}
+
+DWORD
+GetTraceStoresAllocationSize(void)
+{
+    return sizeof(TRACE_STORES);
+}
+
+#ifdef __cpp
+} // extern "C"
+#endif
--- Python/Product/VsPyProf/Tracing.h
+++ Python/Product/VsPyProf/Tracing.h
@@ -16,22 +16,46 @@
 
 #pragma once
 
+#ifdef __cpp
 extern "C" {
+#endif
 
 #include <Windows.h>
+#include "VsPyProf.h"
 
-typedef enum _TraceEvent {
+typedef enum _TraceEventType {
     // PyTrace_* constants.
-    TraceEvent_PyTrace_CALL = 0,
-    TraceEvent_PyTrace_EXCEPTION = 1,
-    TraceEvent_PyTrace_LINE = 2,
-    TraceEvent_PyTrace_RETURN = 3,
-    TraceEvent_PyTrace_C_CALL = 4,
-    TraceEvent_PyTrace_C_EXCEPTION = 5,
-    TraceEvent_PyTrace_C_RETURN = 6,
-} TraceEvent;
+    TraceEventType_PyTrace_CALL = 0,
+    TraceEventType_PyTrace_EXCEPTION = 1,
+    TraceEventType_PyTrace_LINE = 2,
+    TraceEventType_PyTrace_RETURN = 3,
+    TraceEventType_PyTrace_C_CALL = 4,
+    TraceEventType_PyTrace_C_EXCEPTION = 5,
+    TraceEventType_PyTrace_C_RETURN = 6,
+} TraceEventType;
 
-typedef struct _TRACE_EVENT {
+typedef struct _EVENT_TYPE {
+    TraceEventType Id;
+    LPCWSTR Name;
+    LPCSTR  NameA;
+} EVENT_TYPE, *PEVENT_TYPE;
+
+static EVENT_TYPE EventTypes[] = {
+    { TraceEventType_PyTrace_CALL, L"PyTrace_CALL", "PyTrace_CALL" },
+    { TraceEventType_PyTrace_EXCEPTION, L"PyTrace_EXCEPTION", "PyTrace_EXCEPTION" },
+    { TraceEventType_PyTrace_LINE, L"PyTrace_LINE", "PyTrace_LINE" },
+    { TraceEventType_PyTrace_RETURN, L"PyTrace_RETURN", "PyTrace_RETURN" },
+    { TraceEventType_PyTrace_C_CALL, L"PyTrace_C_CALL", "PyTrace_C_CALL" },
+    { TraceEventType_PyTrace_C_EXCEPTION, L"PyTrace_C_EXCEPTION", "PyTrace_C_EXCEPTION" },
+    { TraceEventType_PyTrace_C_RETURN, L"PyTrace_C_RETURN", "PyTrace_C_RETURN" },
+};
+
+static const DWORD NumberOfTraceEventTypes = (
+    sizeof(EventTypes) /
+    sizeof(EVENT_TYPE)
+);
+
+typedef struct _TRACE_EVENT1 {
     USHORT          Version;        //  2   2
     USHORT          EventId;        //  2   4
     DWORD           Flags;          //  4   8
@@ -40,13 +64,101 @@ typedef struct _TRACE_EVENT {
     DWORD           ThreadId;       //  4   24
     DWORD_PTR       Event;          //  8   30
     USHORT          Unused;         //  2   32
+} TRACE_EVENT1, *PTRACE_EVENT1;
+
+typedef struct _TRACE_EVENT {
+    USHORT          Version;            //  2   2
+    USHORT          EventType;          //  2   4
+    DWORD           LineNumber;         //  4   8
+    LARGE_INTEGER   SystemTime;         //  8   16
+    DWORD_PTR       FramePointer;       //  8   24
+    DWORD_PTR       ModulePointer;      //  8   30
+    DWORD_PTR       FuncPointer;        //  8   38
+    DWORD_PTR       LinePointer;        //  8   46
+    DWORD_PTR       ObjPointer;         //  8   54
+    USHORT          SequenceId;         //  2   56
+    DWORD           ProcessId;          //  4   60
+    DWORD           ThreadId;           //  4   64
 } TRACE_EVENT, *PTRACE_EVENT;
 
 typedef struct _PYTRACE_CALL {
+    USHORT Version;
+    DWORD LineNumber;
+    USHORT Unused1;
     DWORD_PTR FrameToken;
     DWORD_PTR ModuleToken;
     DWORD_PTR FunctionToken;
-    DWORD LineNumber;
+    DWORD_PTR LineToken;
 } PYTRACE_CALL, *PPYTRACE_CALL;
 
-}
+typedef struct _TRACE_STORE {
+    HANDLE              FileHandle;
+    HANDLE              MappingHandle;
+    LARGE_INTEGER       MappingSize;
+    FILE_STANDARD_INFO  FileInfo;
+    LPVOID              BaseAddress;
+    LPVOID              NextAddress;
+    LARGE_INTEGER       RecordCount;
+} TRACE_STORE, *PTRACE_STORE;
+
+static const LPCWSTR TraceStoreFileNames[] = {
+    L"trace_events.bin",
+    L"trace_frames.bin",
+    L"trace_modules.bin",
+    L"trace_functions.bin",
+    L"trace_exceptions.bin",
+    L"trace_lines.bin",
+};
+
+static const DWORD NumberOfTraceStores = (
+    sizeof(TraceStoreFileNames) /
+    sizeof(LPCWSTR)
+);
+
+static const DWORD InitialTraceStoreFileSizes[] = {
+    10 << 20,   // events
+    10 << 20,   // frames
+    10 << 20,   // modules
+    10 << 20,   // functions
+    10 << 20,   // exceptions
+    10 << 20,   // lines
+};
+
+typedef struct _TRACE_STORES {
+    union {
+        TRACE_STORE Stores[sizeof(TraceStoreFileNames)/sizeof(LPCWSTR)];
+        struct {
+            TRACE_STORE Events;
+            TRACE_STORE Frames;
+            TRACE_STORE Modules;
+            TRACE_STORE Functions;
+            TRACE_STORE Exceptions;
+            TRACE_STORE Lines;
+        };
+    };
+} TRACE_STORES, *PTRACE_STORES;
+
+VSPYPROF_API BOOL InitializeTraceStores(
+    LPWSTR BaseDirectory,
+    PTRACE_STORES TraceStores,
+    LPDWORD InitialFileSizes
+);
+
+VSPYPROF_API void CloseTraceStore(PTRACE_STORE TraceStore);
+VSPYPROF_API void CloseTraceStores(PTRACE_STORES TraceStores);
+
+VSPYPROF_API DWORD GetTraceStoresAllocationSize(void);
+
+VSPYPROF_API BOOL GetTraceStoreBytesWritten(
+    PTRACE_STORE TraceStore,
+    PLARGE_INTEGER BytesWritten
+);
+
+VSPYPROF_API BOOL GetTraceStoreRecordCount(
+    PTRACE_STORE TraceStore,
+    PLARGE_INTEGER RecordCount
+);
+
+#ifdef __cpp
+} // extern "C"
+#endif
