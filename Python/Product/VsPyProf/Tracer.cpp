098ddf8979aadc5ad76ff1d9300a50c5b30a5feb
Trent Nelson <trent@trent.me>
2015-12-11 16:50:22 -0500

    Checkpoint commit -- work in progress.

---
+++ Python/Product/VsPyProf/Tracer.cpp
@@ -0,0 +1,157 @@
+// Python Tools for Visual Studio
+// Copyright(c) Microsoft Corporation
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the License); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+// IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+//
+// See the Apache Version 2.0 License for specific language governing
+// permissions and limitations under the License.
+
+#include "stdafx.h"
+#include "Tracer.h"
+
+Tracer::Tracer(
+    VsPyProf *Profiler
+) : Profiler(Profiler)
+{
+
+}
+
+Tracer::~Tracer()
+{
+
+}
+
+Tracer* Tracer::Create(
+    VsPyProf *Profiler,
+    LPCWSTR TraceDirectoryPath,
+    LPCWSTR SessionName
+{
+    BOOL Success;
+    FILE_STANDARD_INFO Info;
+    LPCWSTR Path = FileName.c_str();
+    HANDLE FileMappingHandle;
+    PVOID Data;
+    PVOID Buffer;
+    DWORD FileSize;
+    HANDLE FileHandle;
+
+    FileHandle = CreateFile(
+        Path,
+        GENERIC_READ,
+        FILE_SHARE_READ,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_READONLY,
+        NULL
+    );
+
+    if (FileHandle == INVALID_HANDLE_VALUE) {
+        return NULL;
+    }
+
+    Success = GetFileInformationByHandleEx(
+        FileHandle,
+        (FILE_INFO_BY_HANDLE_CLASS)FileStandardInfo,
+        &Info,
+        sizeof(Info)
+    );
+    if (!Success) {
+        CloseHandle(FileHandle);
+        return NULL;
+    }
+
+    // Don't support source files greater than 2GB.
+    if (Info.EndOfFile.HighPart != 0) {
+        CloseHandle(FileHandle);
+        return NULL;
+    }
+    FileSize = Info.EndOfFile.LowPart;
+
+    FileMappingHandle = CreateFileMapping(
+        FileHandle,
+        NULL,
+        PAGE_READONLY,
+        0,
+        0,
+        NULL
+    );
+
+    if (FileMappingHandle == INVALID_HANDLE_VALUE) {
+        CloseHandle(FileHandle);
+        return NULL;
+    }
+
+    Data = MapViewOfFile(FileMappingHandle, FILE_MAP_READ, 0, 0, 0);
+    if (Data == NULL) {
+        CloseHandle(FileMappingHandle);
+        CloseHandle(FileHandle);
+        return NULL;
+    }
+
+    Buffer = VirtualAlloc(0, FileSize, MEM_COMMIT, PAGE_READWRITE);
+    if (Buffer == NULL) {
+        UnmapViewOfFile(Data);
+        CloseHandle(FileMappingHandle);
+        CloseHandle(FileHandle);
+        return NULL;
+    }
+
+    PCHAR Source = (PCHAR)Data;
+    PCHAR Dest = (PCHAR)Buffer;
+    DWORD Lines = 0;
+    DWORD NextLine = 0;
+    DWORD InitialSize = (FileSize >= 30 ? FileSize / 30 : 3);
+
+    vector<DWORD> LineOffsets(InitialSize);
+    // LineOffsets[0] = 0 -> dummy entry.
+    // LineOffsets[1] = 0 -> byte offset of first line will always be 0.
+    LineOffsets[Lines++] = 0;
+    LineOffsets[Lines++] = 0;
+    // Copy the file contents from Source to Dest, converting \r and \n
+    // to \0 as we go (allowing the underlying buffer to be cast as a
+    // NUL-terminated C string).
+    for (DWORD i = 0; i < FileSize; i++) {
+        CHAR c = Source[i];
+        if (c == '\r') {
+            Dest[i] = 0;
+            continue;
+        } else if (c == '\n') {
+            NextLine = i + 1;
+            Dest[i] = 0;
+        } else {
+            Dest[i] = Source[i];
+            continue;
+        }
+
+        if (Lines == LineOffsets.size()) {
+            LineOffsets.resize(LineOffsets.size() + 100);
+        }
+        LineOffsets[Lines++] = i + 1;
+    }
+
+    LineOffsets.resize(Lines);
+
+    UnmapViewOfFile(Data);
+    CloseHandle(FileMappingHandle);
+    CloseHandle(FileHandle);
+
+    SourceFile *pSourceFile = new SourceFile(
+        ModuleToken,
+        ModuleName,
+        FileName,
+        FileSize,
+        (PCHAR)Buffer,
+        Lines,
+        LineOffsets
+    );
+
+    return pSourceFile;
+}
---
+++ Python/Product/VsPyProf/Tracer.h
@@ -0,0 +1,117 @@
+// Python Tools for Visual Studio
+// Copyright(c) Microsoft Corporation
+// All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the License); you may not use
+// this file except in compliance with the License. You may obtain a copy of the
+// License at http://www.apache.org/licenses/LICENSE-2.0
+//
+// THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS
+// OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY
+// IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE,
+// MERCHANTABLITY OR NON-INFRINGEMENT.
+//
+// See the Apache Version 2.0 License for specific language governing
+// permissions and limitations under the License.
+
+#pragma once
+
+#include <Windows.h>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <unordered_map>
+
+using namespace std;
+
+typedef FileFunctionToLineNumberMap unordered_map<tuple<DWORD_PTR, DWORD_PTR>, DWORD>;
+
+class Tracer2 {
+public:
+    void
+    RegisterName(
+        _In_        DWORD_PTR       NameToken,
+        _In_        PCWSTR          Name
+    );
+
+    void
+    RegisterFunction(
+        _In_        DWORD_PTR       FunctionToken,
+        _In_        PCWSTR          FunctionName,
+        _In_        DWORD           LineNumber,
+        _In_opt_    DWORD_PTR       ModuleToken,
+        _In_opt_    PCWSTR          ModuleName,
+        _In_opt_    PCWSTR          ModuleFilename
+    );
+
+    void
+    RegisterModule(
+        _In_        DWORD_PTR       ModuleToken,
+        _In_        PCWSTR          ModuleName,
+        _In_        PCWSTR          ModuleFilename
+    );
+};
+
+
+class Tracer {
+    VsPyProf *Profiler;
+
+    HANDLE TraceDirectory;
+    HANDLE TraceFileHandle;
+
+    unordered_map<tuple<DWORD_PTR, DWORD_PTR>, DWORD> FileFuncToLine;
+    unordered_map<DWORD_PTR, LPCWSTR> Names;
+
+    Tracer(VsPyProf *Profiler);
+
+    Tracer& operator=(const Tracer&) { }
+    Tracer(const Tracer&) { }
+
+public:
+    static Tracer* Create(
+        VsPyProf *Profiler
+    );
+    ~Tracer();
+
+    void SourceLine(
+        DWORD_PTR FunctionToken,
+        DWORD_PTR FileToken,
+        DWORD LineNumber
+    );
+
+    void NameToken(
+        DWORD_PTR Token,
+        LPCWSTR Name
+    );
+
+    void TraceCall(
+        PyFrameObject *Frame,
+        DWORD_PTR FunctionToken,
+        DWORD_PTR ModuleToken
+    );
+    void TraceException(PyFrameObject *Frame, PyObject *Exception);
+    void TraceLine(PyFrameObject *Frame);
+    void TraceReturn(
+        PyFrameObject *Frame,
+        DWORD_PTR FunctionToken,
+        DWORD_PTR ModuleToken,
+    );
+    void TraceCCall(
+        PyFrameObject *Frame,
+        DWORD_PTR FunctionToken,
+        DWORD_PTR ModuleToken
+    );
+
+    void SourceLine(
+        DWORD_PTR FunctionToken,
+        DWORD_PTR FileToken,
+        DWORD LineNumber
+    );
+
+    void NameToken(
+        DWORD_PTR Token,
+        LPCWSTR Name
+    );
+
+};
+
